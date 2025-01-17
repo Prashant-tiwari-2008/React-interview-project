87af1c108a89e0f1399546a111e773ebe8919907
Prashant tiwari <prashanttiwari.vns@gmail.com>
2025-01-17 07:02:43 +0530

    tic-tac game completed

--- RoadsiteCoder/tic-tac-toe/src/components/Board.jsx
+++ RoadsiteCoder/tic-tac-toe/src/components/Board.jsx
@@ -6,7 +6,7 @@ const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
         const nextSquares = squares.slice();
@@ -18,6 +18,15 @@ const Board = () => {
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
     }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner : " + winner; 
+    } else {
+        status = "Next player : " + (xIsNext ? "X" : "O")
+    }
+
 
     function calculateWinner(squares) {
         const lines = [
@@ -42,6 +51,7 @@ const Board = () => {
 
     return (
         <div>
+            <div className='status'>{status}</div>
             <div className='board-row'>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
